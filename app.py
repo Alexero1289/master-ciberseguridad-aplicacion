5d599eb21e904be65eb2b279ff4562a5739e3c7f
Alexero1289 <136047935+Alexero1289@users.noreply.github.com>
2023-06-12 23:29:57 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,55 @@
+from flask import Flask, Response
 import requests
+import json
+import logging
 
-def search(objeto: str):
-    url = f"https://api.wallapop.com/api/v3/general/search?filters_source=search_box&keywords={objeto}&longitude=-3.69196&latitude=40.41956"
-    user_agent = {'User-agent': 'Mozilla/5.0'}
-    r = requests.get(url, headers = user_agent)
-    results = r.json()["search_objects"]
-    for r in results:
-        print(f'{r["price"]}€ -  {r["title"]}')
+app = Flask(__name__)
 
-search (¨consulta¨)
+
+
+logging.basicConfig(level=logging.DEBUG)
+
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+
+@app.route("/get-price/<ticker>")
+def get_price(ticker):
+    url = f"https://query2.finance.yahoo.com/v10/finance/quoteSummary/{ticker}?modules=price%2CsummaryDetail%2CpageViews%2CfinancialsTemplate"
+    response = requests.get(url)
+    company_info = response.json()
+    app.logger.info(f"Requested ticker: {ticker}")
+
+    if response.status_code > 400:
+        app.logger.info(f"Yahoo has problem with ticker: {ticker}.")
+        app.logger.info(f"Yahoo status code: {response.status_code}.")
+        return Response({}, status=404, mimetype='application/json')
+
+    app.logger.info(company_info)
+
+    try:
+        price = company_info['quoteSummary']['result'][0]['price']['regularMarketPrice']['raw']
+        company_name = company_info['quoteSummary']['result'][0]['price']['longName']
+        exchange = company_info['quoteSummary']['result'][0]['price']['exchangeName']
+        currency = company_info['quoteSummary']['result'][0]['price']['currency']
+
+        result = {
+            "price": price,
+            "name": company_name,
+            "exchange": exchange,
+            "currency": currency
+        }
+        app.logger.info(result)
+
+        return Response(json.dumps(result), status=200, mimetype='application/json')
+    except (KeyError, TypeError):
+        return Response({}, status=404, mimetype='application/json')
+    except Exception as e:
+        app.logger.error("Exception occurred", exc_info=True)
+
+
+if __name__ == '__main__':
+    app.run()
+
